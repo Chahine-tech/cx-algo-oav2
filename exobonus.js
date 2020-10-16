1263ebe47fca07d19fd8947fed0999562b052862
Chahine Benlahcen <chahine.benlahcen@gmail.com>
2020-10-16 15:08:04 +0200

    bonus

--- exobonus.js
+++ exobonus.js
@@ -1,10 +1,10 @@
 function selectionSorting(arr) {
 
-    for (var i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
 
         let min = i; //  stockage de l'index 
 
-        for (var j = i + 1; j < arr.length; j++) {
+        for (let j = i + 1; j < arr.length; j++) {
             if (arr[min] > arr[j]) {
                 min = j; 
             }
